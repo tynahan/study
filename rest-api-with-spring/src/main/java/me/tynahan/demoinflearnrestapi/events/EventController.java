4ba5ab59cadd42602756a88bebc8ed914ec4b40f
youngwoo.nam <tynahan@gmail.com>
2019-10-10 16:25:17 +0900

    POST /api/events returns 201

---
+++ rest-api-with-spring/src/main/java/me/tynahan/demoinflearnrestapi/events/EventController.java
@@ -0,0 +1,29 @@
+package me.tynahan.demoinflearnrestapi.events;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping(value="/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
+public class EventController {
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        Event newEvent = this.eventRepository.save(event);
+        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        return ResponseEntity.created(createdUri).body(event);
+
+    }
+}
--- rest-api-with-spring/src/test/java/me/tynahan/demoinflearnrestapi/events/EventControllerTests.java
+++ rest-api-with-spring/src/test/java/me/tynahan/demoinflearnrestapi/events/EventControllerTests.java
@@ -1,15 +1,23 @@
 package me.tynahan.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,11 +27,36 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 10, 10, 14, 16))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 10, 11, 14, 16))
+                .beginEventDateTime(LocalDateTime.of(2019, 10, 12, 14, 16))
+                .endEventDateTime(LocalDateTime.of(2019, 10, 13, 14, 16))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("상암 누리꿈 스퀘어")
+                .build();
+
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
         mockMvc.perform(post("/api/events/")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
