859658f9fc5afac8fe8e6ca427fd6b180809fc8c
youngwoo.nam <tynahan@gmail.com>
2019-10-15 09:49:09 +0900

    Body for BadRequest

--- rest-api-with-spring/src/main/java/me/tynahan/demoinflearnrestapi/events/EventValidator.java
+++ rest-api-with-spring/src/main/java/me/tynahan/demoinflearnrestapi/events/EventValidator.java
@@ -11,8 +11,9 @@ public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors) {
         if ((eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0)) {
-            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
-            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+//            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
+//            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+            errors.reject("wrongPrices", "Values for prices are wrong");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
