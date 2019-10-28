f615bb24491fda3ceb1aea59a8b06e5b9f74380f
youngwoo.nam <tynahan@gmail.com>
2019-10-28 12:51:26 +0900

    Form authentication

---
+++ rest-api-with-spring/src/main/java/me/tynahan/demoinflearnrestapi/configs/AppConfig.java
@@ -0,0 +1,55 @@
+package me.tynahan.demoinflearnrestapi.configs;
+
+import me.tynahan.demoinflearnrestapi.accounts.Account;
+import me.tynahan.demoinflearnrestapi.accounts.AccountRole;
+import me.tynahan.demoinflearnrestapi.accounts.AccountService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account tynahan = Account.builder()
+                        .email("tynahan@gmail.com")
+                        .password("dbgndbgn")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(tynahan);
+            }
+        };
+    }
+}
+
+
+
+
+
+
+
