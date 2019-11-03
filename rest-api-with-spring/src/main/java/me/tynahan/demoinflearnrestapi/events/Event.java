25d37c89d026b73e21d055984711b6532d0685ae
youngwoo.nam <tynahan@gmail.com>
2019-11-03 12:54:06 +0900

    Use AccountSerializer

--- rest-api-with-spring/src/main/java/me/tynahan/demoinflearnrestapi/events/Event.java
+++ rest-api-with-spring/src/main/java/me/tynahan/demoinflearnrestapi/events/Event.java
@@ -1,7 +1,9 @@
 package me.tynahan.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 import me.tynahan.demoinflearnrestapi.accounts.Account;
+import me.tynahan.demoinflearnrestapi.accounts.AccountSerializer;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -28,6 +30,7 @@ public class Event {
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
     @ManyToOne
+    @JsonSerialize(using = AccountSerializer.class)
     private Account manager;
 
     public void update() {
