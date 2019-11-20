c7f7a9f96585f49caee6509a647a236ef01f7a5e
youngwoo nam <tynahan@gmail.com>
2019-11-20 09:05:25 +0900

    First Commit

---
+++ jpa-with-spring/src/main/java/me/tynahan/demoinflearnjpa/DemoInflearnJpaApplication.java
@@ -0,0 +1,13 @@
+package me.tynahan.demoinflearnjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInflearnJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInflearnJpaApplication.class, args);
+    }
+
+}
