1c0a6a65c7bbd6208a17e7b71679cdb17cabb690
youngwoo nam <tynahan@gmail.com>
2019-11-25 00:05:06 +0900

    Chore: First Commit

---
+++ h2-file-based-db-with-spring-jpa/src/main/java/me/tynahan/demoh2filebaseddb/DemoH2FileBasedDbApplication.java
@@ -0,0 +1,13 @@
+package me.tynahan.demoh2filebaseddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoH2FileBasedDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoH2FileBasedDbApplication.class, args);
+    }
+
+}
