0a2122020bebf04eeea21cd5acea6a91aa3791e8
youngwoo nam <tynahan@gmail.com>
2019-11-28 00:14:37 +0900

    Chore: Add Instt Controller

---
+++ instt-info-manage/src/main/java/me/tynahan/demoinsttinfomanage/controller/InsttInfoController.java
@@ -0,0 +1,8 @@
+package me.tynahan.demoinsttinfomanage.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class InsttInfoController {
+
+}
---
+++ instt-info-manage/src/test/java/me/tynahan/demoinsttinfomanage/controller/InsttInfoControllerTest.java
@@ -0,0 +1,18 @@
+package me.tynahan.demoinsttinfomanage.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InsttInfoControllerTest {
+
+    @Test
+    public void testGetInsttInfos() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+}
