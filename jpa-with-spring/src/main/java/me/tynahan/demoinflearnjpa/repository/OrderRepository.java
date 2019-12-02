09ea5914555e4ac0b4af1ba1f0f1f31090ada2f7
youngwoo nam <tynahan@gmail.com>
2019-12-02 12:43:52 +0900

    Feature: Modify Order Search with jpql

--- jpa-with-spring/src/main/java/me/tynahan/demoinflearnjpa/repository/OrderRepository.java
+++ jpa-with-spring/src/main/java/me/tynahan/demoinflearnjpa/repository/OrderRepository.java
@@ -24,11 +24,38 @@ public class OrderRepository {
     }
 
     public List<Order> findOrderByCond(OrderSearch orderSearch) {
-        return em.createQuery("select o from Order o join o.member m" +
-                " where o.status = :status" +
-                " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .getResultList();
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
     }
 }
