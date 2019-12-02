7c949758e8f2f697a179ac6c1e4318c3d78a3f93
youngwoo nam <tynahan@gmail.com>
2019-12-02 18:33:35 +0900

    Feature: Add Home Controller and template and static resource

---
+++ jpa-with-spring/src/test/java/me/tynahan/demoinflearnjpa/controller/HomeControllerTest.java
@@ -0,0 +1,34 @@
+package me.tynahan.demoinflearnjpa.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testHome() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
