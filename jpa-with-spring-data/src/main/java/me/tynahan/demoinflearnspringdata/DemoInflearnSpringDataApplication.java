72a804c33b127072fa5b0aa9feab6d0af7e4ed5e
youngwoo nam <tynahan@gmail.com>
2019-12-10 21:23:13 +0900

    최초 커밋

---
+++ jpa-with-spring-data/src/main/java/me/tynahan/demoinflearnspringdata/DemoInflearnSpringDataApplication.java
@@ -0,0 +1,13 @@
+package me.tynahan.demoinflearnspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInflearnSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInflearnSpringDataApplication.class, args);
+    }
+
+}
---
+++ jpa-with-spring-data/src/test/java/me/tynahan/demoinflearnspringdata/DemoInflearnSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tynahan.demoinflearnspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInflearnSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
