dec000273b88f802c38e8024bb363e74e2c33ab3
youngwoo nam <tynahan@gmail.com>
2019-12-12 21:38:31 +0900

    basic jpa reposiotry 구현

---
+++ jpa-with-spring-data/src/main/java/me/tynahan/demoinflearnspringdata/repository/TeamRepository.java
@@ -0,0 +1,43 @@
+package me.tynahan.demoinflearnspringdata.repository;
+
+import me.tynahan.demoinflearnspringdata.entity.Team;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class).getResultList();
+    }
+
+    public Team find(Long id) {
+        return em.find(Team.class, id);
+    }
+
+    public Optional<Team> findById(Long id) {
+        Team team = em.find(Team.class, id);
+        return Optional.ofNullable(team);
+    }
+
+    public Long count() {
+        return em.createQuery("select count(t) from Team t", Long.class).getSingleResult();
+    }
+
+}
